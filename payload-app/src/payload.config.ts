518f37b1edade8436d5a74951611d9102b55760c
Alex Russell <alexrusselldev@gmail.com>
2023-08-04 16:59:17 +0100

    feat: ✨ add s3 bucket config

--- payload-app/src/payload.config.ts
+++ payload-app/src/payload.config.ts
@@ -2,6 +2,20 @@ import { buildConfig } from "payload/config";
 import path from "path";
 // import Examples from './collections/Examples';
 import Users from "./collections/Users";
+import { s3Adapter } from "@payloadcms/plugin-cloud-storage/s3";
+import { cloudStorage } from "@payloadcms/plugin-cloud-storage";
+import Media from "./collections/Media";
+
+const adapter = s3Adapter({
+  config: {
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY_ID,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    },
+    region: process.env.S3_REGION,
+  },
+  bucket: process.env.S3_BUCKET,
+});
 
 export default buildConfig({
   serverURL: process.env.SERVER_URL,
@@ -10,6 +24,7 @@ export default buildConfig({
   },
   collections: [
     Users,
+    Media,
     // Add Collections here
     // Examples,
   ],
@@ -19,4 +34,17 @@ export default buildConfig({
   graphQL: {
     schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
   },
+  plugins: [
+    cloudStorage({
+      collections: {
+        media: {
+          adapter,
+          generateFileURL: (args) => {
+            return `${process.env.S3_ENDPOINT}/${process.env.S3_BUCKET}/${args.filename}`;
+          },
+          disableLocalStorage: true,
+        },
+      },
+    }),
+  ],
 });
