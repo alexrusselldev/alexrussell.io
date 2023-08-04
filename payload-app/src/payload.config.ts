b95690abc0586cec7284e27b253064b7dc7aac8a
Alex Russell <alexrusselldev@gmail.com>
2023-08-04 15:27:55 +0100

    feat: ✨ update subdomain locations

--- payload-app/src/payload.config.ts
+++ payload-app/src/payload.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import Users from "./collections/Users";
 
 export default buildConfig({
-  serverURL: `https://admin.alexrussell.io`,
+  serverURL: `https://dashboard.alexrussell.io`,
   admin: {
     user: Users.slug,
   },
