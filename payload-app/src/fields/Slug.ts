f77ff944cf12ae2a33e82fe7dc78b51a3def30c1
Alex Russell <alexrusselldev@gmail.com>
2023-08-07 11:21:09 +0100

    feat: ✨ add slug field with automatic formatting

---
+++ payload-app/src/fields/Slug.ts
@@ -0,0 +1,20 @@
+import { Field } from "payload/types";
+import formatSlug from "../utilities/formatSlug";
+import deepMerge from "../utilities/deepMerge";
+
+type Slug = (fieldToUse?: string, overrides?: Partial<Field>) => Field;
+
+export const slugField: Slug = (fieldToUse = "title", overrides) =>
+  deepMerge<Field, Partial<Field>>(
+    {
+      name: "slug",
+      label: "Slug",
+      type: "text",
+      unique: true,
+      index: true,
+      hooks: {
+        beforeValidate: [formatSlug(fieldToUse)],
+      },
+    },
+    overrides
+  );
